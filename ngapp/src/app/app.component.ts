4aec2d58b20ac15c86528e2be685633ce6a8f92c
Юрий <yurialekseev@mail.ru>
2019-04-04 17:55:03 +0300

    added request func and subscription

--- ngapp/src/app/app.component.ts
+++ ngapp/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import { interval, Observable, fromEvent } from 'rxjs';
+// import { of } from 'rxjs/internal/observable/of';
+// import { zip } from 'rxjs/internal/observable/zip';
+ import { catchError, mergeMap, concatMap } from 'rxjs/operators';
+ import { empty } from 'rxjs/internal/observable/empty';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ngapp';
+
+ngAfterViewInit() {
+  const searchInput = document.getElementsByClassName('search-input')[0] as HTMLInputElement;
+
+const inputSequence$ = fromEvent(
+  searchInput,
+  'keyup'
+)
+.pipe(mergeMap(searchValue => performedRequst(searchValue)))
+.subscribe(user => {
+         console.log(user);
+});
+
+function performedRequst(search: string) {
+     return fetch('https://api.github.com/users/' + search)
+         .then(res => res.json());
+}
+
+
+}
+
 }
--- ngapp/src/app/app.module.ts
+++ ngapp/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { interval, Observable, fromEvent } from 'rxjs';
+// import { of } from 'rxjs/internal/observable/of';
+// import { zip } from 'rxjs/internal/observable/zip';
+ import { catchError, mergeMap } from 'rxjs/operators';
+ import { empty } from 'rxjs/internal/observable/empty';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -16,3 +22,25 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+const searchInput = document.getElementsByClassName('search-input')[0] as HTMLInputElement;
+
+const inputSequence$ = fromEvent(
+  searchInput,
+  'keyup'
+)
+.pipe(mergeMap(_keyup => performedRequst()))
+.subscribe(user => {
+         console.log(user.email);
+});
+
+function performedRequst() {
+     return fetch('https://jsonplaceholder.typicode.com/users/1')
+         .then(res => res.json());
+}
+
+/*
+
+class User{
+  email: string;
+}*/
