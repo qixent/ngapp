18b11bd5cfb4f5f9799762ffa7180cb746bed91a
Юрий <yurialekseev@mail.ru>
2019-04-07 12:11:26 +0300

    search by repo

--- ngapp/src/app/app.component.ts
+++ ngapp/src/app/app.component.ts
@@ -6,13 +6,19 @@ import { interval, Observable, fromEvent } from 'rxjs';
  import { catchError, mergeMap, concatMap } from 'rxjs/operators';
  import { empty } from 'rxjs/internal/observable/empty';
 
+ class Repo {
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements AfterViewInit {
   title = 'ngapp';
+  repoArray: Array<Repo>;
 
 ngAfterViewInit() {
   const searchInput = document.getElementsByClassName('search-input')[0] as HTMLInputElement;
@@ -21,16 +27,16 @@ const inputSequence$ = fromEvent(
   searchInput,
   'keyup'
 )
-.pipe(mergeMap(searchValue => performedRequst(searchValue)))
-.subscribe(user => {
-         console.log(user);
+.pipe(mergeMap(keyup => performedRequst(searchInput.value)))
+.subscribe(searchResult => {
+    this.repoArray = searchResult.items;
+         //console.log(searchResult.items[0]);
 });
 
 function performedRequst(search: string) {
-     return fetch('https://api.github.com/users/' + search)
+     return fetch('https://api.github.com/search/repositories?sort=stars&order=desc&q=' + search)
          .then(res => res.json());
 }
-
 
 }
 
--- ngapp/src/app/app.module.ts
+++ ngapp/src/app/app.module.ts
@@ -1,12 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { interval, Observable, fromEvent } from 'rxjs';
-// import { of } from 'rxjs/internal/observable/of';
-// import { zip } from 'rxjs/internal/observable/zip';
- import { catchError, mergeMap } from 'rxjs/operators';
- import { empty } from 'rxjs/internal/observable/empty';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -23,24 +17,6 @@ import { AppComponent } from './app.component';
 })
 export class AppModule { }
 
-const searchInput = document.getElementsByClassName('search-input')[0] as HTMLInputElement;
-
-const inputSequence$ = fromEvent(
-  searchInput,
-  'keyup'
-)
-.pipe(mergeMap(_keyup => performedRequst()))
-.subscribe(user => {
-         console.log(user.email);
-});
-
-function performedRequst() {
-     return fetch('https://jsonplaceholder.typicode.com/users/1')
-         .then(res => res.json());
+export class Repo {
+  repoName: string;
 }
-
-/*
-
-class User{
-  email: string;
-}*/
